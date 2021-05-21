14a97fca7f5b9c0b85cd765b3d8bebee3b969cf9
Jinho1011 <gino9940@gmail.com>
2021-05-22 03:17:16 +0900

    init

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  StatusBar,
+  Text,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+export default App;
