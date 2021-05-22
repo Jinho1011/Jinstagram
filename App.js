5fadf91e71d57eff98ab62d0a7b33dfe5d91c108
jinho1011 <gino9940@gmail.com>
2021-05-23 01:28:01 +0900

    add Header, Post, PostContainer, Nav

--- App.js
+++ App.js
@@ -1,10 +1,44 @@
 import React from 'react';
 import {StatusBar, Text, View} from 'react-native';
 
+const Header = () => {
+  return (
+    <View>
+      <Text>THIS IS HEADER</Text>
+    </View>
+  );
+};
+
+const Post = () => {
+  return (
+    <View>
+      <Text>THIS IS POST</Text>
+    </View>
+  );
+};
+
+const PostContainer = () => {
+  return (
+    <View>
+      <Post></Post>
+    </View>
+  );
+};
+
+const Navigation = () => {
+  return (
+    <View>
+      <Text>This is Nav</Text>
+    </View>
+  );
+};
+
 const App = () => {
   return (
     <View>
-      <Text>Hello</Text>
+      <Header></Header>
+      <PostContainer></PostContainer>
+      <Navigation></Navigation>
     </View>
   );
 };
